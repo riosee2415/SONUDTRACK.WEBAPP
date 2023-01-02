b9a29e6a2296f56eba1365fb7e073eee16ca3862
khj6407 <khj64076407@gmail.com>
2023-01-02 16:37:54 +0900

    commit

--- front/components/Theme.js
+++ front/components/Theme.js
@@ -1,7 +1,7 @@
 export default {
-  basicTheme_C: "#FFB940",
+  basicTheme_C: "#531F7C",
 
-  subTheme_C: "#31A5F0",
+  subTheme_C: "#FCF5FC",
   subTheme2_C: "#106BDC",
   subTheme3_C: "#2D53A3",
   subTheme4_C: "#FFD321",
@@ -16,9 +16,11 @@ export default {
   // red
   red_C: "#e74c3c",
   // grey
-  grey_C: "#747474",
-  lightGrey_C: "#E2E2E2",
-  darkGrey_C: "#2d3436",
+  grey_C: "#7A7A7A",
+  grey2_C: "#B5B5B5",
+  lightGrey_C: "#E4E4E4",
+  lightGrey2_C: "#F9F9FB",
+  darkGrey_C: "#4B4B4B",
   //admin
   adminTheme_1: "#1e272e",
   adminTheme_2: "#485460",
