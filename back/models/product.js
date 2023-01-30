ef07ed454a84626fe1260174e66512fa359002b0
hmg11123 <4leaf.hmg@gmail.com>
2023-01-30 17:26:23 +0900

    modify

--- back/models/product.js
+++ back/models/product.js
@@ -51,6 +51,12 @@ module.exports = class Product extends Model {
           allowNull: false,
           defaultValue: false,
         },
+        hit: {
+          // 좋아요
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 0,
+        },
       },
       {
         modelName: "Product",
